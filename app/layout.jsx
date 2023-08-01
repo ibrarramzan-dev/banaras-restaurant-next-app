f9bc2393bb0be30caf3839f5fe8b6d0b03032dc2
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-08-01 11:27:22 +0500

    ui(animations): update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
-import "./globals.css";
+"use client";
+import { useRef, useEffect } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import "./globals.css";
 
 export const metadata = {
   title: "Banaras xpress",
@@ -8,12 +11,54 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
     <html lang="en">
       <body>
         <main>
           <Header />
-          {children}
+          <div
+            ref={ref}
+            style={{
+              position: "relative",
+              width: "100%",
+              overflow: "hidden",
+            }}
+          >
+            <motion.div
+              variants={{
+                hidden: { opacity: 0 },
+                visible: { opacity: 1 },
+              }}
+              initial="hidden"
+              animate={mainControls}
+              transition={{ duration: 1, delay: 0.1 }}
+            >
+              {children}
+            </motion.div>
+            {/* <motion.div
+              variants={{
+                hidden: { left: 0 },
+                visible: { left: "100%" },
+              }}
+              initial="hidden"
+              animate={slideControls}
+              transition={{ duration: 5, ease: "easeIn" }}
+\            >
+              {children}
+            </motion.div> */}
+          </div>{" "}
           <Footer />
         </main>
       </body>
