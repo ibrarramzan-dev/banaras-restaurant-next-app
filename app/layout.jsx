83d34261166e42bf7c2fc5d080bc46260c168262
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-08-02 20:47:51 +0500

    ui(layout): update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,3 @@
-"use client";
-import { useRef, useEffect } from "react";
-import { motion, useInView, useAnimation } from "framer-motion";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -11,42 +8,12 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-
-  const mainControls = useAnimation();
-
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start("visible");
-    }
-  }, [isInView]);
-
   return (
     <html lang="en">
       <body>
         <main>
           <Header />
-          <div
-            ref={ref}
-            style={{
-              position: "relative",
-              width: "100%",
-              overflow: "hidden",
-            }}
-          >
-            <motion.div
-              variants={{
-                hidden: { opacity: 0 },
-                visible: { opacity: 1 },
-              }}
-              initial="hidden"
-              animate={mainControls}
-              transition={{ duration: 1, delay: 0.1 }}
-            >
-              {children}
-            </motion.div>
-          </div>{" "}
+          {children}
           <Footer />
         </main>
       </body>
